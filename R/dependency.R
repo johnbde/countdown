4f2a7d5d9ee89637a7fe45b4a14af53fddc1050a
johnbde <44228842+johnbde@users.noreply.github.com>
2021-02-27 13:50:53 +0530

    Delete dependency.R

--- R/dependency.R
+++
@@ -1,8 +0,0 @@
-customjs_html_dependency <- function() {
-  htmltools::htmlDependency(
-    name = "tutorial-customjs",
-    version = "0.1",
-    src = c(href = "countdown/customjs"),
-    script = "customjs.js"
-  )
-}
