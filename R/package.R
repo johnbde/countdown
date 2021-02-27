54132e5b84cc22bacc7d5b0b1683d6674d50de91
johnbde <44228842+johnbde@users.noreply.github.com>
2021-02-27 13:49:01 +0530

    Update package.R

--- R/package.R
+++ R/package.R
@@ -1,11 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-
-# addResourcePath('tutorial-customjs', system.file('customjs', package='countdown'))
-
-    htmltools::htmlDependency(
-    name = "tutorial-customjs",
-    version = "0.1",
-    src = c(href = "countdown/customjs"),
-    script = "customjs.js"
-  )
+shiny::addResourcePath('tutorial-customjs', system.file('customjs', package='countdown'))
 }
