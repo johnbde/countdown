b40bbc84728eded9fa285c5726c4dab68087d839
johnbde <44228842+johnbde@users.noreply.github.com>
2021-02-28 17:04:12 +0530

    Update countdown.R

--- R/countdown.R
+++ R/countdown.R
@@ -231,8 +231,8 @@ countdown <- function(
  # file.copy(system.file("customjs.js", package = "countdown"),
  #           file.path(tmpdir, "customjs.js"))
   
- file.copy(system.file("smb_stage_clear.mp3", package = "countdown"),
-            file.path(tmpdir, "smb_stage_clear.mp3"))
+ # file.copy(system.file("smb_stage_clear.mp3", package = "countdown"),
+ #           file.path(tmpdir, "smb_stage_clear.mp3"))
   
 
   # Set text based on background color
